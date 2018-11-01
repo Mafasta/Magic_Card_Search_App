09aa4b483fcaa9f964a2f17a6ff11d613ecd3347
Thomas Langsfeld <tqlangsfeld@gmail.com>
2018-11-01 08:41:48 -0600

    implemented searching for cards

--- MTGApp/app/controllers/mtg_card_controller.rb
+++ MTGApp/app/controllers/mtg_card_controller.rb
@@ -2,7 +2,7 @@ class MtgCardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-  	@mtg_cards = MtgCard.all
+  	@mtg_cards = MtgCard.where(name: params[:card_name]).all
   end
 
   def create
