81b02d42252483b6f7892c122d763b354e6b316f
Thomas Langsfeld <tqlangsfeld@gmail.com>
2018-11-27 15:21:05 -0700

    project part 5 video. yard docs. minor app code changes.

--- MTGApp/app/controllers/mtg_card_controller.rb
+++ MTGApp/app/controllers/mtg_card_controller.rb
@@ -2,6 +2,6 @@ class MtgCardController < ApplicationController
   # compiles list of MtgCards and CardLists
   def index
   	@mtg_cards = MtgCard.text_search(params[:card_name])
-  	@card_lists = CardList.all
+  	@card_lists = CardList.where(general_user_id: params[:user_id]).all if params[:user_id]
   end
 end
--- MTGApp/app/controllers/user_profile_controller.rb
+++ MTGApp/app/controllers/user_profile_controller.rb
@@ -1,9 +1,22 @@
 class UserProfileController < ApplicationController
 	# compiles deck and favorite lists for a GeneralUser
+	# @return [CardList, CardList, MtgCard, MtgCard] the user's deck and favorite lists and the corresponding mtg cards
 	def show
-		@deck_list = DeckList.where(general_user_id: params[:id]).first
-		@favorite_list = FavoriteList.where(general_user_id: params[:id]).first
+		@deck_list = deck_list
+		@favorite_list = favorite_list
 		@deck = DeckListManager.new.manage_list(@deck_list.id)
 		@favorites = FavoriteListManager.new.manage_list(@favorite_list.id)
+	end
+
+	private
+
+	# @return [DeckList] the user's deck list
+	def deck_list
+		DeckList.where(general_user_id: params[:id]).first
+	end
+
+	# @return [FavoriteList] the user's favorite list
+	def favorite_list
+		FavoriteList.where(general_user_id: params[:id]).first
 	end
 end
